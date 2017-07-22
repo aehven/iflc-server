28ccf61eb7c98421c95b2f63c4632625b4fc00fb
Adam Ehven <aehven@binarytrees.biz>
2017-07-22 17:45:13 -0600

    added cee, flavor, and note models

---
+++ app/models/cee.rb
@@ -0,0 +1,64 @@
+require 'csv'
+
+class AccountSerializer < ActiveModel::Serializer
+  attributes :id,
+             :name,
+             :form,
+             :source
+end
+
+class Cee < ApplicationRecord
+
+  enum form: {
+    gas: 100,
+    liquid: 200,
+    mush: 300,
+    solid: 400
+  }, _prefix: true
+
+  enum source: {
+    animal: 100,
+    vegitable: 200,
+    mineral: 300
+  }, _prefix: true
+  
+  has_many :flavors, dependent: :destroy
+  has_many :notes, dependent: :destroy
+
+  # http://stackoverflow.com/a/21818014
+  has_many :favorites, as: :favoritable
+
+  default_scope {order(name: :asc)}
+
+  scope :search, -> term {
+    where("name like ? or form like ? or source like ?",
+    "%#{term}%", "%#{term}%", "%#{term}%")
+  }
+
+  def self.to_csv(options = {})
+    columns = [
+      'name',
+      'form',
+      'source'
+    ]
+
+    CSV.generate(options) do |csv|
+      header = [
+        'Name',
+        'Form',
+        'Source'
+      ]
+
+      csv.add_row(header)
+
+      all.each do |account|
+        row = []
+        row += [account.name]
+        row += [account.form]
+        row += [account.source]
+
+        csv.add_row(row)
+      end
+    end
+  end
+end
---
+++ app/models/flavor.rb
@@ -0,0 +1,24 @@
+class ContactSerializer < ActiveModel::Serializer
+  attributes  :id,
+              :name,
+              :color,
+              :cee_name
+
+  def cee_name
+    object.cee&.name
+  end
+end
+
+class Flavor < ApplicationRecord
+  belongs_to :cee
+
+  # http://stackoverflow.com/a/21818014
+  has_many :favorites, as: :favoritable
+
+  default_scope {order(last_name: :asc).order(first_name: :asc)}
+
+  scope :search, -> term {
+    where("name like ? or color like",
+    "%#{term}%", "%#{term}%")
+  }
+end
