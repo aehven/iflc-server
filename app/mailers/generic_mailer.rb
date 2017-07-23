9c4e1d7bc91b91aef6df009583cde89934199a7b
Adam Ehven <aehven@binarytrees.biz>
2017-07-22 20:00:10 -0600

    replace controlers

--- app/mailers/generic_mailer.rb
+++ app/mailers/generic_mailer.rb
@@ -1,6 +1,6 @@
 class GenericMailer < ApplicationMailer
   def email_report(user, xls)
     attachments["report.xls"] = {mime_type: "application/xls", content: xls}
-    mail(to: user.email, subject: "Accounts Report")
+    mail(to: user.email, subject: "Cees Report")
   end
 end
